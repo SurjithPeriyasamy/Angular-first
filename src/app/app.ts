4dc0fa86a60be8e4356e71f6f39ced3da45c4276
Surjith <mahindhiran005@gmail.com>
2025-06-26 23:43:43 +0530

    shows dummy dynamic user with click event

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header';
-import { User } from './user/user';
+import { UserComponent } from './user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, User],
+  imports: [RouterOutlet, HeaderComponent, UserComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
+const randomUser = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.html',
-  styleUrl: './user.css'
+  styleUrl: './user.css',
 })
-export class User {
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomUser];
 
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    const randomUser = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomUser];
+  }
 }
