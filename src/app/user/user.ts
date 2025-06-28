bddc6328b4d7b4a6919c371b1b233ee1a526dc9a
Surjith <mahindhiran005@gmail.com>
2025-06-28 16:58:43 +0530

    accept Object as inputs with type

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -17,15 +17,16 @@ const randomUser = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: {
+    id: string;
+    name: string;
+    avatar: string;
+  };
   @Output() select = new EventEmitter();
-  // select = output<string>();
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
