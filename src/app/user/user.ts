61998cc0fae7179694ec514cc38b833ecc152104
Surjith <mahindhiran005@gmail.com>
2025-06-28 15:45:42 +0530

    Ouputs and emiting data

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,11 @@
-import { Component, computed, input, Input, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  Output,
+  signal,
+} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomUser = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -9,17 +16,14 @@ const randomUser = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
-
-  avatar = input.required<string>();
-  name = input.required<string>();
-
-  imagePath = computed(() => 'assets/users/' + this.avatar());
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar;
-  // }
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
   onSelectUser() {
-    const randomUser = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.select.emit(this.id);
   }
 }
