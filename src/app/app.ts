e5b3ca9a55533526f58e2472296f865d7a03eb2c
Surjith <mahindhiran005@gmail.com>
2025-06-28 18:48:10 +0530

    using structural directives *ngFor & *ngIf

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,16 +4,24 @@ import { HeaderComponent } from './header/header';
 import { UserComponent } from './user/user';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    UserComponent,
+    TasksComponent,
+    NgFor,
+    NgIf,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId: string = '';
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
